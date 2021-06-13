10dfa2d6593365a815876ce5af45a61d5ac01579
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-06-13 04:31:57 -0400

    merged raja, updated animations

--- GMTK GameJam 2021/Assets/Scripts/StateMachine/Hammond/H_CheckAnimationFinishedDecision.cs
+++ GMTK GameJam 2021/Assets/Scripts/StateMachine/Hammond/H_CheckAnimationFinishedDecision.cs
@@ -2,7 +2,5 @@
 
 [CreateAssetMenu(menuName = "PluggableAI/Decisions/Hammond/CheckIfAnimationFinishedDecision")]
     public class H_CheckAnimationFinishedDecision : Decision<Hammond> {
-        public override bool Decide(Hammond type) {
-            
-        }
+        public override bool Decide(Hammond type) => type.isAnimationFinished;
     }
--- GMTK GameJam 2021/Assets/Scripts/StateMachine/Hammond/Hammond.cs
+++ GMTK GameJam 2021/Assets/Scripts/StateMachine/Hammond/Hammond.cs
@@ -5,22 +5,21 @@ public class Hammond : MonoBehaviour, IDamageable {
     private HammondBaseState currentState;
 
     [SerializeField] private HammondBaseState remainState;
-    
-    [Header("Components")]
-    public Animator anim; 
-    public Rigidbody2D rb; 
-    public SpriteRenderer sr; 
+
+    [Header("Components")] public Animator anim;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
     public new BoxCollider2D collider;
 
     [HideInInspector] public int facingDirection;
     [HideInInspector] public Vector2 currentVelocity;
     [HideInInspector] public bool isAnimationFinished;
-    
-    [Header("Enemy Info")]
-    public float movementSpeed;
+
+    [Header("Enemy Info")] public float movementSpeed;
     public float rayDistance;
     public LayerMask playerLayer;
-    
+    public float health = 100f;
+
     private void Awake() {
         facingDirection = -1;
     }
@@ -53,11 +52,14 @@ public class Hammond : MonoBehaviour, IDamageable {
 
     public void AnimationFinishTrigger() => currentState.AnimationFinishTrigger(this); // Used as an Animation Event
 
-    public bool IsDetectingPlayer() => Physics2D.Raycast(collider.bounds.center, Vector2.right * facingDirection, rayDistance, playerLayer);
+    public bool IsDetectingPlayer() => Physics2D.Raycast(collider.bounds.center, Vector2.right * facingDirection,
+        rayDistance, playerLayer);
+
     public void Flip() {
         facingDirection *= -1;
         transform.Rotate(0f, -180f, 0f);
     }
+
     public int TakeDamage(float damage) {
         if (health > 0f) {
             health -= damage;
@@ -69,11 +71,13 @@ public class Hammond : MonoBehaviour, IDamageable {
             Destroy(gameObject, 0.1f);
             return 2; // hammond is 2 index for animation override controller in Player class
         }
+    }
+
     private void OnDrawGizmos() {
         Gizmos.color = Color.black;
-        
+
         var bounds = collider.bounds;
         Gizmos.DrawWireSphere(bounds.center, 0.5f);
-        Gizmos.DrawLine(bounds.center, bounds.center + (Vector3)(Vector2.right * facingDirection * rayDistance));
+        Gizmos.DrawLine(bounds.center, bounds.center + (Vector3) (Vector2.right * facingDirection * rayDistance));
     }
 }
