b6e29906d072b6cde595f434acbf7cd037b60c1a
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-06-15 19:56:26 -0400

    yee
    haw

---
+++ GMTK GameJam 2021/Assets/Resources/StateMachine/Player/Cock.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Cock", menuName = "Cock")]
+public class Cock : State<Player>
+{
+    public override void OnEnter(Player type) {
+    }
+
+    public override void LogicUpdate(Player type) {
+    }
+
+    public override void OnExit(Player type) {
+    }
+}
+
+
