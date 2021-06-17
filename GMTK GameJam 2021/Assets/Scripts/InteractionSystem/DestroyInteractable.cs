facda22d1d226f350dc47c8804fe66e57f9bb5d7
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-06-16 22:12:11 -0400

    interactable

---
+++ GMTK GameJam 2021/Assets/Scripts/InteractionSystem/DestroyInteractable.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class DestroyInteractable : InteractableBase {
+    public override void OnInteract() {
+        base.OnInteract();
+        Destroy(gameObject);
+    }
+}
---
+++ GMTK GameJam 2021/Assets/Scripts/Utils/Interfaces.cs
@@ -0,0 +1,7 @@
+﻿public interface IDamageable {
+    int TakeDamage(float damage);
+}
+
+// public interface ISpecial {
+//     void Activate(T ability);
+// }
